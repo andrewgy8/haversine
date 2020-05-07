d7ee18e5d30278d18b5b3dfcf1a9a3cd915e9523
andrewgy8 <andrewgy8@gmail.com>
2020-05-07 21:46:37 +0200

    Split into lib and bin

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use std::f64;
+
+static R: f64 = 6372.8;
+
+pub struct Point {
+    pub lat: f64,
+    pub lon: f64,
+}
+
+pub fn haversine(mut origin: Point, mut destination: Point) -> f64 {
+    origin.lon -= destination.lon;
+    origin.lon = origin.lon.to_radians();
+    origin.lat = origin.lat.to_radians();
+    destination.lat = destination.lat.to_radians();
+    let dz: f64 = origin.lat.sin() - destination.lat.sin();
+    let dx: f64 = origin.lon.cos() * origin.lat.cos() - destination.lat.cos();
+    let dy: f64 = origin.lon.sin() * origin.lat.cos();
+    ((dx * dx + dy * dy + dz * dz).sqrt() / 2.0).asin() * 2.0 * R
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let origin: Point = Point {
+            lat: 36.12,
+            lon: -86.67,
+        };
+        let destination: Point = Point {
+            lat: 33.94,
+            lon: -118.4,
+        };
+
+        assert_eq!(haversine(origin, destination), 2887.2599506071106);
+    }
+}
