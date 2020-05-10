7693bc063f244688c58993491199fe45897ade62
andrewgy8 <andrewgy8@gmail.com>
2020-05-10 22:17:41 +0200

    Many to many matrix calculation

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub struct Measure {
     pub distance: f64,
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct Matrix {
     pub distances: Vec<f64>,
 }
@@ -21,16 +21,22 @@ impl Matrix {
     pub fn add_distance(&mut self, measure: Measure) {
         self.distances.push(measure.distance);
     }
+
+    pub fn add_distances(&mut self, distances: Vec<f64>) {
+        for distance in distances {
+            self.distances.push(distance);
+        }
+    }
 }
 
-pub fn haversine(mut origin: Point, mut destination: Point) -> Measure {
-    origin.lon -= destination.lon;
-    origin.lon = origin.lon.to_radians();
-    origin.lat = origin.lat.to_radians();
-    destination.lat = destination.lat.to_radians();
-    let dz: f64 = origin.lat.sin() - destination.lat.sin();
-    let dx: f64 = origin.lon.cos() * origin.lat.cos() - destination.lat.cos();
-    let dy: f64 = origin.lon.sin() * origin.lat.cos();
+pub fn haversine(origin: Point, destination: Point) -> Measure {
+    let diff = origin.lon - destination.lon;
+    let ori_lon = diff.to_radians();
+    let ori_lat = origin.lat.to_radians();
+    let dest_lat = destination.lat.to_radians();
+    let dz: f64 = ori_lat.sin() - dest_lat.sin();
+    let dx: f64 = ori_lon.cos() * ori_lat.cos() - dest_lat.cos();
+    let dy: f64 = ori_lon.sin() * ori_lat.cos();
     let dist: f64 = ((dx * dx + dy * dy + dz * dz).sqrt() / 2.0).asin() * 2.0 * R;
     Measure { distance: dist }
 }
@@ -42,6 +48,25 @@ pub fn one_to_many(origin: Point, destinations: Vec<Point>) -> Matrix {
     for destination in destinations {
         let measure = haversine(origin, destination);
         matrix.add_distance(measure);
+    }
+    matrix
+}
+
+fn one_to_many_with_origin(origin: Point, destinations: Vec<Point>) -> Matrix {
+    let mut matrix: Matrix = Matrix::default();
+    for destination in destinations {
+        let measure = haversine(origin, destination);
+        matrix.add_distance(measure);
+    }
+    matrix
+}
+
+pub fn many_to_many(destinations: Vec<Point>) -> Matrix {
+    let mut matrix: Matrix = Matrix::default();
+    let copy_dest = destinations.iter();
+    for origin in copy_dest {
+        let sub_matrix = one_to_many_with_origin(*origin, destinations.clone());
+        matrix.add_distances(sub_matrix.distances);
     }
     matrix
 }
@@ -99,6 +124,40 @@ mod tests {
         assert_eq!(
             matrix.distances,
             [0.0, 58.604658305034434, 9665.831848220572]
+        );
+    }
+
+    #[test]
+    fn it_works_for_many_to_many() {
+        let mut destinations: Vec<Point> = Vec::new();
+        destinations.push(Point {
+            lat: 39.472978,
+            lon: -0.375203,
+        });
+
+        destinations.push(Point {
+            lat: 38.967161,
+            lon: -0.184759,
+        });
+        destinations.push(Point {
+            lat: 33.94,
+            lon: -118.4,
+        });
+
+        let matrix = many_to_many(destinations);
+        assert_eq!(
+            matrix.distances,
+            [
+                0.0,
+                58.604658305034434,
+                9665.831848220572,
+                58.60465830503454,
+                0.0,
+                9716.147344492676,
+                9665.831848220572,
+                9716.147344492676,
+                0.0
+            ]
         );
     }
 }
